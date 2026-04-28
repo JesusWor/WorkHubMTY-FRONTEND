a34ae8679b3283b74886d36f9ffb2b8bb3421db5
cristian-rla <Cristianrluquea@hotmail.com>
2026-04-28 11:28:25 -0600

    first prototype cubicle pages

--- src/app/(private)/reservaciones/page.tsx
+++ src/app/(private)/reservaciones/page.tsx
@@ -1,22 +1,33 @@
 import Calendar from "@/app/components/Calendar/Calendar";
 import HourSelect from "@/app/components/HourSelect/HourSelect";
-import ScrollAgenda from "@/app/components/ScrollAgenda/ScrollAgenda";
+import Agenda from "@/app/components/Agenda/Agenda";
 import Link from "next/link";
+import DailyEventCard from "@/app/components/DailyEventCard/DailyEventCard";
+import agendaEvents from "./events.json";
 
 export default function Reservaciones() {
   return (
     <div className="w-full min-h-full bg-background-page flex flex-col flex-1 px-12 py-8 gap-4">
       <div className="flex">
         <span className="text-3xl">Ajusta tu reservación</span>
-        <label className="flex gap-2 ml-auto bg-container px-4 py-2 rounded-lg">
+        <label className="flex gap-2 ml-auto bg-container px-4 py-2 rounded-lg items-center">
           <span className="material-symbols-outlined">settings</span>
-          Preconfiguración
+          <span className="text-sm">Preconfiguración</span>
         </label>
       </div>
-      <ScrollAgenda />
-      <div className="flex-1 flex flex-row gap-4">
-        <Calendar />
-        <HourSelect />
+      <div className="flex flex-row gap-10 ">
+        <div className="flex flex-col gap-4">
+          <Agenda
+            events={agendaEvents}
+            day={new Date(new Date().setHours(0, 0, 0, 0))}
+          />
+          <DailyEventCard events={[...agendaEvents.espacioReservado]} />
+        </div>
+
+        <div className="flex-1 flex flex-col gap-4">
+          <Calendar />
+          <HourSelect />
+        </div>
       </div>
       <Link href="/cubiculo" className="self-end">
         <button className="bg-primary-1 text-on-primary-1 px-4 py-2 rounded-lg">
--- src/app/components/Calendar/Calendar.tsx
+++ src/app/components/Calendar/Calendar.tsx
@@ -1,3 +1,52 @@
-export default function Calendar() {
-  return <div className="grid bg-container flex-2"></div>;
+"use client";
+
+function generateDays(startingDay: Date, interval: number) {
+  const lastDay = new Date();
+  lastDay.setDate(startingDay.getDate() + interval);
+  if (startingDay > lastDay) {
+    console.log(startingDay);
+    console.log(lastDay);
+    throw new Error("starting date must be before the end of the interval");
+  }
+
+  const days: Date[] = [];
+  for (let i = 0; i < 21; i++) {
+    const currentDay = new Date(startingDay);
+    currentDay.setDate(currentDay.getDate() + i);
+    days.push(currentDay);
+  }
+
+  return days;
+}
+
+export default function Calendar({
+  startingDay = new Date(),
+}: {
+  startingDay: Date;
+}) {
+  const days = generateDays(new Date(), 21);
+  const WEEY_DAYS = ["D", "L", "M", "M", "J", "V", "S"];
+  return (
+    <div className="grid grid-cols-[repeat(7, minmax(0, 1fr))] grid-rows-[repeat(6, minmax(0, 1fr))] gap-1 p-2 bg-container rounded">
+      {WEEY_DAYS.map((day, index) => (
+        <span
+          key={index}
+          className="flex items-center justify-center p-2 row-start-1 font-bold select-none"
+        >
+          {day}
+        </span>
+      ))}
+      {days.map((day) => (
+        <span
+          key={String(day)}
+          className="flex justify-self-center self-center items-center justify-center p-2 h-8 w-8 select-none rounded-full hover:bg-primary-1 hover:text-on-primary-1 cursor-pointer"
+          style={{
+            gridColumn: day.getDay() + 1,
+          }}
+        >
+          {day.getDate()}
+        </span>
+      ))}
+    </div>
+  );
 }
